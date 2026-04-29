8447ad1abb7bdfe416c6bfc1792e8f47b95f3701
Maeva ZERBIB <164505530+Kuro-Neko13@users.noreply.github.com>
2026-04-29 17:03:36 +0200

    Ajout du fichier controleur python

---
+++ Base_de_donnees/controleur.py
@@ -0,0 +1,85 @@
+import os
+import sys
+import subprocess
+import json
+
+if len(sys.argv)!=2:                                             #Respectivement if len(sys.argv)!=3:
+    sys.exit("Usage : python ./controleur.py metadonnees.json")       #sys.exit("Usage : python ./controleur.py mesure.csv/xlsx metadonnees.json")
+
+#Si l'on veut séparer le chemin du fichier de mesure du fichier en json :
+#arg1 = sys.argv[1]  #Argument 1, le chemin du fichier de mesure, exemple : r".\Base_de_donnees\data_95224601_2024_11_19_0.csv"
+#arg2 = sys.argv[2]  #Argument 2, le chemin du fichier de métadonnées, exemple : r".\Base_de_donnees\metadonnees.json"
+#Sinon :
+arg = sys.argv[1]    # Argument, le chemin du fichier de métadonnées, exemple : r".\Base_de_donnees\metadonnees.json"
+
+with open(arg, encoding="utf-8") as f:
+    metadonnees = json.load(f)                 #Ouverture du json
+    match metadonnees["script"]:
+        case "intégration":                    #Cas où le script à éxécuter est le script d'intégration
+            try:
+                retour = subprocess.run(["python", "./Base_de_donnees/integration_donnees_"+metadonnees["nom_outil"].lower()+".py", arg], shell=True, capture_output=True, text=True, check=True)
+                #éxécution du script en permettant de stocker la valeur de "retour"(les print)
+                print(retour.stdout)           #"Retour" de notre script si tout s'est bien passé
+            except subprocess.CalledProcessError as e:
+                print(f"La commande d'intégration des données a échoué avec le code d'erreur : {e.returncode}")
+                #"Retour" de notre script si tout ne s'est pas bien passé
+                #print(e.stderr)            #Si l'on veut voir tout le message d'erreur effectuer par le script défectueux
+                #sys.exit(e.stdout)         #Si l'on veut voir tout les print effectuer par le script défectueux
+        case "vérification":                   #Cas où le script à éxécuter est le script de vérification
+            try:
+                retour = subprocess.run(["python", "./Base_de_donnees/verification_donnees_"+metadonnees["nom_outil"].lower()+".py", arg], shell=True, capture_output=True, text=True, check=True)
+                print(retour.stdout)           #"Retour" de notre script si tout s'est bien passé
+            except subprocess.CalledProcessError as e:
+                print(f"La commande de vérification des données a échoué avec le code d'erreur : {e.returncode}")
+                #"Retour" de notre script si tout ne s'est pas bien passé
+                #print(e.stderr)            #Si l'on veut voir tout le message d'erreur effectuer par le script défectueux
+                #sys.exit(e.stdout)         #Si l'on veut voir tout les print effectuer par le script défectueux
+        case "intégration_métadonnées":
+            args = []
+            for i in range(len(metadonnees["type_script"])):
+                match metadonnees["type_script"][i]:
+                    case "personne":
+                        args.append(f"--ficPers {metadonnees["fichier_données"][i]}")
+                    case "capteur":
+                        args.append(f"--ficInstr {metadonnees["fichier_données"][i]}")
+                    case "localisation":
+                        args.append("--ficLoc")
+                        args.append(metadonnees["fichier_données"][i])
+                    case "projet":
+                        if "personne" not in metadonnees["type_script"]:
+                            sys.exit("Pas d'intégration de projet sans intégration de personne")
+                        args.append(f"--ficProj {metadonnees["fichier_données"][i]}")
+            try:
+                retour = subprocess.run(["python", "./integration_metadonnees.py"]+args, shell=True, capture_output=True, text=True, check=True)
+                #éxécution du script en permettant de stocker la valeur de "retour"(les print)
+                print(retour.stdout)           #"Retour" de notre script si tout s'est bien passé
+            except subprocess.CalledProcessError as e:
+                print(f"La commande d'intégration des métadonnées a échoué avec le code d'erreur : {e.returncode}")
+                #"Retour" de notre script si tout ne s'est pas bien passé
+                #print(e.stderr)               #Si l'on veut voir tout le message d'erreur effectuer par le script défectueux
+                #sys.exit(e.stdout)            #Si l'on veut voir tout les print effectuer par le script défectueux
+        case "vérification_métadonnées":
+            args = []
+            for i in range(len(metadonnees["type_script"])):
+                match metadonnees["type_script"][i]:
+                    case "personne":
+                        args.append(f"--ficPers {metadonnees["fichier_données"][i]}")
+                    case "capteur":
+                        args.append(f"--ficInstr {metadonnees["fichier_données"][i]}")
+                    case "localisation":
+                        args.append(f"--ficLoc {metadonnees["fichier_données"][i]}")
+                    case "projet":
+                        if "personne" not in metadonnees["type_script"]:
+                            sys.exit("Pas de vérification de projet sans vérification de personne")
+                        args.append(f"--ficProj {metadonnees["fichier_données"][i]}")
+            try:
+                retour = subprocess.run(["python", "./Base_de_donnees/verification_metadonnees.py"]+args, shell=True, capture_output=True, text=True, check=True)
+                #éxécution du script en permettant de stocker la valeur de "retour"(les print)
+                print(retour.stdout)           #"Retour" de notre script si tout s'est bien passé
+            except subprocess.CalledProcessError as e:
+                print(f"La commande de vérification des métadonnées a échoué avec le code d'erreur : {e.returncode}")
+                #"Retour" de notre script si tout ne s'est pas bien passé
+                #print(e.stderr)               #Si l'on veut voir tout le message d'erreur effectuer par le script défectueux
+                #sys.exit(e.stdout)            #Si l'on veut voir tout les print effectuer par le script défectueux
+
+    
